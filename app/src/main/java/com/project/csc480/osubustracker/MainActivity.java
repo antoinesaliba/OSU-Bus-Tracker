624dcb91e289fe2db88b4e9691fcbdeb807bc811
Lucas Neubert <lneubert@oswego.edu>
2015-02-13 13:15:04 -0500

    Route Highlighting (Blue Route)
    Blue Route is highlighted properly. I just uploaded the
    MainActivity.java
    Any problems, tell me.

--- app/src/main/java/com/project/csc480/osubustracker/MainActivity.java
+++ app/src/main/java/com/project/csc480/osubustracker/MainActivity.java
@@ -31,15 +31,33 @@ public class MainActivity extends FragmentActivity{
     private GoogleMap mMap; // Might be null if Google Play services APK is not available.
     ArrayList<LatLng> markerPoints;
 
+    private static final LatLng CAMPUS_CENTER = new LatLng(43.453838, -76.540628);
+    private static final LatLng CIRCLE = new LatLng(43.453523, -76.541181);
+    //private static final LatLng CAMPUS_CENTER = new LatLng(43.453793, -76.540730); //43.453933, -76.540344   43.453838, -76.540628
+    private static final LatLng ONONDAGA = new LatLng(43.450535, -76.549731);
+    private static final LatLng RIGGS_HALL = new LatLng(43.457295865792744, -76.53929114341736);
+    private static final LatLng VILLAGE = new LatLng(43.44699935247679, -76.54906511306763);
+    private static final LatLng PENFIELD_LIBRARY = new LatLng(43.454309, -76.543996);
+    private static final LatLng SHINEMAN = new LatLng(43.454282, -76.539160);
+
+    private static final LatLng MARY_WALKER = new LatLng(43.455475, -76.542743);
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
         markerPoints = new ArrayList<LatLng>();
         setUpMapIfNeeded();
 
+
+
+
         // Getting reference to SupportMapFragment of the activity_main
         SupportMapFragment fm = (SupportMapFragment)getSupportFragmentManager().findFragmentById(R.id.map);
+        Log.i("MainActivity", "Setup passed...");
     }
 
     @Override
@@ -85,14 +103,12 @@ public class MainActivity extends FragmentActivity{
 
     //sets the location for the map as well as how far in to zoom (right now zoom set to 15)
     private void setUpMap(GoogleMap map) {
-        LatLng oswego = new LatLng(43.453854, -76.540654);
+
 
         //map.setMyLocationEnabled(true);
-        map.moveCamera(CameraUpdateFactory.newLatLngZoom(oswego, 15));
+        map.moveCamera(CameraUpdateFactory.newLatLngZoom(CAMPUS_CENTER, (float)14.5));
         map.setMapType(GoogleMap.MAP_TYPE_SATELLITE);
-        LatLng point1 = new LatLng(43.451156, -76.547953);
-        LatLng village = new LatLng(43.457295865792744, -76.53929114341736);
-        LatLng point2 = new LatLng(43.44699935247679, -76.54906511306763);
+
 
         // Already 10 locations with 8 waypoints and 1 start location and 1 end location.
         // Up to 8 waypoints are allowed in a query for non-business users
@@ -101,26 +117,47 @@ public class MainActivity extends FragmentActivity{
         }
 
         // Adding new item to the ArrayList
-        markerPoints.add(oswego);
-        markerPoints.add(point1);
-        markerPoints.add(village);
-        markerPoints.add(point2);
+        //Origin:
+        markerPoints.add(CAMPUS_CENTER);
+
+        //Destination:
+        markerPoints.add(CAMPUS_CENTER);
+
+        //Waypoints:
+        markerPoints.add(CIRCLE);
+        markerPoints.add(RIGGS_HALL);
+        markerPoints.add(ONONDAGA);
+        markerPoints.add(VILLAGE);
+        markerPoints.add(PENFIELD_LIBRARY);
+        markerPoints.add(SHINEMAN);
+
 
 
         // Creating MarkerOptions
-        MarkerOptions options = new MarkerOptions();
+        MarkerOptions markerDest = new MarkerOptions();
+        MarkerOptions markerOrigin = new MarkerOptions();
+
+        MarkerOptions busStop = new MarkerOptions();
 
         // Setting the position of the marker
-        options.position(oswego);
-        options.position(village);
-        options.position(point1);
-        options.position(point2);
 
-        /**
-         * For the start location, the color of marker is GREEN and
-         * for the end location, the color of marker is RED and
-         * for the rest of markers, the color is AZURE
-         */
+        markerOrigin.position(CAMPUS_CENTER)  //Origin
+                    .title("Origin")
+                    .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_AZURE));
+
+        markerDest.position(CAMPUS_CENTER) //Destination
+                  .title("Destination")
+                  .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_RED));
+
+        busStop.position(MARY_WALKER) //Destination
+                .title("Bus Stop: Mary Walker")
+                .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_ORANGE));
+
+                /**
+                 * For the start location, the color of marker is GREEN and
+                 * for the end location, the color of marker is RED and
+                 * for the rest of markers, the color is AZURE
+                 */
         if (markerPoints.size() >= 2) {
             LatLng origin = markerPoints.get(0);
             LatLng dest = markerPoints.get(1);
@@ -136,7 +173,9 @@ public class MainActivity extends FragmentActivity{
 
 
         // Add new marker to the Google Map Android API V2
-        map.addMarker(options);
+        map.addMarker(markerDest);
+        map.addMarker(markerOrigin);
+        map.addMarker(busStop);
     }
     private String getDirectionsUrl(LatLng origin,LatLng dest){
 
@@ -154,8 +193,9 @@ public class MainActivity extends FragmentActivity{
         for(int i=2;i<markerPoints.size();i++){
             LatLng point  = (LatLng) markerPoints.get(i);
             if(i==2)
-                waypoints = "waypoints=";
-            waypoints += point.latitude + "," + point.longitude + "|";
+             //   waypoints = "waypoints=optimize:false|";
+                  waypoints = "waypoints=";
+            waypoints +=  point.latitude + "," + point.longitude + "|";
         }
 
         // Building the parameters to the web service
@@ -292,8 +332,8 @@ public class MainActivity extends FragmentActivity{
 
                 // Adding all the points in the route to LineOptions
                 lineOptions.addAll(points);
-                lineOptions.width(2);
-                lineOptions.color(Color.RED);
+                lineOptions.width(6);
+                lineOptions.color(Color.BLUE);
             }
 
             // Drawing polyline in the Google Map for the i-th route
